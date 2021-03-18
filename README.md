ae0c605d335e6a109d03f25fda6042b8ffc4ac75
mizutama6 <pajama.blog0+github20@gmail.com>
2021-03-18 12:52:32 +0900

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+[Build a CHAT APP in VUE and FIREBASE using Vue's Composition API
+](https://www.youtube.com/watch?v=72Fk9i9HcZM)
+を視聴して作成したものです。
---
+++ src/db.js
@@ -0,0 +1,15 @@
+import firebase from 'firebase/app'
+import 'firebase/database'
+
+const config = {
+  apiKey: process.env.VUE_APP_APIKEY,
+  authDomain: process.env.VUE_APP_AUTHDOMAIN,
+  projectId: process.env.VUE_APP_PROJECTID,
+  storageBucket: process.env.VUE_APP_STORAGEBUCKET,
+  messagingSenderId: process.env.VUE_APP_MESSAGINGSENDERID,
+  appId: process.env.VUE_APP_APPID
+}
+
+const db = firebase.initializeApp(config)
+
+export default db
